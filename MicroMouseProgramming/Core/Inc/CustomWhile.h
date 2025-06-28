580622cb9e9619dcc98e310c22164c2a0848e4a3
JesseJabezArendse <ARNJES009@myuct.ac.za>
2025-06-28 14:18:42 +0200

    massive updaate

--- MicroMouseProgramming/Core/Inc/CustomWhile.h
+++
@@ -1,4 +0,0 @@
-#include "stm32l4xx.h"
-#include "main.h"
-
-void CustomWhile(void);
--- runMeFirst.m
+++
@@ -1,5 +0,0 @@
-IR_LED_PERIOD = 0.05;
-signalWindowLength = 20;
-wheelR = 32.5e-3;
-axleLength = 12e-2;
-TIM3_ARR = 4000;
